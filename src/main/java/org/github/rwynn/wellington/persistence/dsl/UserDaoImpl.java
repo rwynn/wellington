6934b9fbe8e0ff73c9759b0a551e00acd8ebc6c2
Ryan Wynn <ryan.m.wynn@gmail.com>
2014-04-18 18:41:14 -0400

    upgrade jooq and cleanup dao

--- src/main/java/org/github/rwynn/wellington/persistence/dsl/UserDaoImpl.java
+++ src/main/java/org/github/rwynn/wellington/persistence/dsl/UserDaoImpl.java
@@ -2,8 +2,7 @@ package org.github.rwynn.wellington.persistence.dsl;
 
 import org.github.rwynn.wellington.rest.RESTPage;
 import org.github.rwynn.wellington.transfer.UserDTO;
-import org.jooq.DSLContext;
-import org.jooq.SelectQuery;
+import org.jooq.*;
 import org.jooq.impl.DSL;
 import org.springframework.data.domain.Pageable;
 import org.springframework.jdbc.core.RowCallbackHandler;
@@ -16,9 +15,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
-import static org.jooq.impl.DSL.*;
 import static org.github.rwynn.wellington.persistence.dsl.PagingConstants.*;
-import static org.github.rwynn.wellington.persistence.jooq.Tables.*;
+import static org.github.rwynn.wellington.persistence.jooq.Tables.AUTHORITIES;
+import static org.github.rwynn.wellington.persistence.jooq.Tables.USERS;
+import static org.jooq.impl.DSL.count;
+import static org.jooq.impl.DSL.param;
 
 @Component
 public class UserDaoImpl extends JooqDao implements UserDao {
@@ -27,16 +28,7 @@ public class UserDaoImpl extends JooqDao implements UserDao {
         final RESTPage<UserDTO> page = new RESTPage<UserDTO>();
         page.setContent(new ArrayList<UserDTO>());
 
-        DSLContext context = getDslContext();
-        SelectQuery query = context.select(USERS.USERNAME, USERS.LOCKED, AUTHORITIES.AUTHORITY,
-                count(USERS.USERNAME).over().as(COUNT)).
-                from(USERS).
-                join(AUTHORITIES).
-                on(USERS.USERNAME.eq(AUTHORITIES.USERNAME)).
-                where(AUTHORITIES.AUTHORITY.eq(param(AUTHORITIES.AUTHORITY.getName(), String.class))).
-                orderBy(USERS.USERNAME.asc()).
-                limit(DSL.param(LIMIT, Integer.class)).
-                offset(DSL.param(OFFSET, Integer.class)).getQuery();
+        SelectQuery query = selectUserQuery(authorityEq());
 
         query.bind(AUTHORITIES.AUTHORITY.getName(), authority);
         query.bind(LIMIT, pageable.getPageSize());
@@ -44,24 +36,14 @@ public class UserDaoImpl extends JooqDao implements UserDao {
 
         final Map<String, UserDTO> users = new HashMap<String, UserDTO>();
 
-        PagingRowCallbackHandler pagingRowCallbackHandler = new PagingRowCallbackHandler(page, pageable, new RowCallbackHandler() {
+        PagingRowCallbackHandler pagingRowCallbackHandler =
+                new PagingRowCallbackHandler(page, pageable, new RowCallbackHandler() {
             @Override
             public void processRow(ResultSet rs) throws SQLException {
                 boolean locked = rs.getBoolean(USERS.LOCKED.getName());
                 String username = rs.getString(USERS.USERNAME.getName());
                 String authority = rs.getString(AUTHORITIES.AUTHORITY.getName());
-                UserDTO userDTO = users.get(username);
-                if (userDTO != null) {
-                    userDTO.getAuthorities().add(authority);
-                } else {
-                    userDTO = new UserDTO();
-                    userDTO.setAuthorities(new HashSet<String>());
-                    userDTO.setUsername(username);
-                    userDTO.setLocked(locked);
-                    userDTO.getAuthorities().add(authority);
-                    page.getContent().add(userDTO);
-                    users.put(username, userDTO);
-                }
+                processUser(locked, username, authority, page, users);
             }
         });
 
@@ -74,16 +56,7 @@ public class UserDaoImpl extends JooqDao implements UserDao {
         final RESTPage<UserDTO> page = new RESTPage<UserDTO>();
         page.setContent(new ArrayList<UserDTO>());
 
-        DSLContext context = getDslContext();
-        SelectQuery query = context.select(USERS.USERNAME, USERS.LOCKED, AUTHORITIES.AUTHORITY,
-                count(USERS.USERNAME).over().as(COUNT)).
-                from(USERS).
-                join(AUTHORITIES).
-                on(USERS.USERNAME.eq(AUTHORITIES.USERNAME)).
-                where(USERS.USERNAME.likeIgnoreCase(param(USERS.USERNAME.getName(), String.class))).
-                orderBy(USERS.USERNAME.asc()).
-                limit(DSL.param(LIMIT, Integer.class)).
-                offset(DSL.param(OFFSET, Integer.class)).getQuery();
+        SelectQuery query = selectUserQuery(userNameLike());
 
         query.bind(USERS.USERNAME.getName(), username);
         query.bind(LIMIT, pageable.getPageSize());
@@ -91,29 +64,58 @@ public class UserDaoImpl extends JooqDao implements UserDao {
 
         final Map<String, UserDTO> users = new HashMap<String, UserDTO>();
 
-        PagingRowCallbackHandler pagingRowCallbackHandler = new PagingRowCallbackHandler(page, pageable, new RowCallbackHandler() {
+        PagingRowCallbackHandler pagingRowCallbackHandler =
+                new PagingRowCallbackHandler(page, pageable, new RowCallbackHandler() {
             @Override
             public void processRow(ResultSet rs) throws SQLException {
                 boolean locked = rs.getBoolean(USERS.LOCKED.getName());
                 String username = rs.getString(USERS.USERNAME.getName());
                 String authority = rs.getString(AUTHORITIES.AUTHORITY.getName());
-                UserDTO userDTO = users.get(username);
-                if (userDTO != null) {
-                    userDTO.getAuthorities().add(authority);
-                } else {
-                    userDTO = new UserDTO();
-                    userDTO.setAuthorities(new HashSet<String>());
-                    userDTO.setUsername(username);
-                    userDTO.setLocked(locked);
-                    userDTO.getAuthorities().add(authority);
-                    page.getContent().add(userDTO);
-                    users.put(username, userDTO);
-                }
+                processUser(locked, username, authority, page, users);
             }
         });
 
         jdbcTemplate.query(query.getSQL(), pagingRowCallbackHandler);
 
         return page;
+    }
+
+    protected SelectQuery<Record4<String, Boolean, String, Integer>>
+        selectUserQuery(Condition condition) {
+        DSLContext context = getDslContext();
+        return context.select(USERS.USERNAME, USERS.LOCKED, AUTHORITIES.AUTHORITY,
+                count(USERS.USERNAME).over().as(COUNT)).
+                from(USERS).
+                join(AUTHORITIES).
+                on(USERS.USERNAME.eq(AUTHORITIES.USERNAME)).
+                where(condition).
+                orderBy(USERS.USERNAME.asc()).
+                limit(DSL.param(LIMIT, Integer.class)).
+                offset(DSL.param(OFFSET, Integer.class)).
+                getQuery();
+    }
+
+    protected Condition authorityEq() {
+        return AUTHORITIES.AUTHORITY.eq(param(AUTHORITIES.AUTHORITY.getName(), String.class));
+    }
+
+    protected Condition userNameLike() {
+        return USERS.USERNAME.likeIgnoreCase(param(USERS.USERNAME.getName(), String.class));
+    }
+
+    protected final void processUser(boolean locked, String username, String authority,
+                                     RESTPage<UserDTO> page, Map<String, UserDTO> users) {
+        UserDTO userDTO = users.get(username);
+        if (userDTO != null) {
+            userDTO.getAuthorities().add(authority);
+        } else {
+            userDTO = new UserDTO();
+            userDTO.setAuthorities(new HashSet<String>());
+            userDTO.setUsername(username);
+            userDTO.setLocked(locked);
+            userDTO.getAuthorities().add(authority);
+            page.getContent().add(userDTO);
+            users.put(username, userDTO);
+        }
     }
 }
